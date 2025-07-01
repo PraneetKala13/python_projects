b9d4c4bbbc8103cc2550ec54f61f88a6d490bd51
Praneet Kala <pj.kala333@gmail.com>
2025-07-01 12:51:34 +0200

    Dice rolling game - Final Commit

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -11,3 +11,23 @@ while check != "n":
     else:
         print("Invalid choice")  # Message for invalid choice
 print("Thanks for playing")  # Message for ending the game
+
+"""
+Could make it robust and check for uppercase and lowercase
+Ensure that the entered string character always changes to lowercase
+Don't use the same variable "check" in the start, use a Boolean
+
+Mosh's final code
+while True:
+    choice = input('Roll the dice? (y/n): ').lower()
+    if choice := 'y': ==
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        print(f'({die1}, {die2})')
+    elif choice 'n':
+        print('Thanks for playing!')
+        break
+    else:
+        print('Invalid choice!')
+
+"""
