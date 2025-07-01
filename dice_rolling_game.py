80da632792be384ddeaa3176f26dcff0f04272c1
Praneet Kala <pj.kala333@gmail.com>
2025-07-01 12:47:19 +0200

    Dice rolling game - Added comments

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -3,11 +3,11 @@ import random
 
 check = "y"
 while check != "n":
-    check = input("Roll the dice? (y/n):")
-    if check == "y":
-        random1 = random.randint(1, 6)
-        random2 = random.randint(1, 6)
-        print(f"({random1}, {random2})")
+    check = input("Roll the dice? (y/n):")  # Ask use if they want to roll dice
+    if check == "y":  # Check if the response is valid
+        random1 = random.randint(1, 6)  # Generate first random number
+        random2 = random.randint(1, 6)  # Generate second random number
+        print(f"({random1}, {random2})")  # Display the numebr
     else:
-        print("Invalid choice")
-print("Thanks for playing")
+        print("Invalid choice")  # Message for invalid choice
+print("Thanks for playing")  # Message for ending the game
