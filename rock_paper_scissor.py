9aab854408c2604202cd65788f13a8d3fd96a100
Praneet Kala <pj.kala333@gmail.com>
2025-07-01 15:26:17 +0200

    Rock, paper, scissor game - Final working code

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,5 +1,7 @@
 import random
 
+# This function will compare results and choose a winner
+
 
 def get_winner(player, computer):
     if player == computer:
@@ -11,6 +13,8 @@ def get_winner(player, computer):
     else:
         return "You lose."
 
+# This is the main function that does most of the work
+
 
 def main():
     choices = {"r": "Rock 🪨", "p": "Paper 📄", "s": "Scissors ✂️"}
@@ -18,15 +22,18 @@ def main():
     while True:
         player = input("Choose Rock (r), Paper (p), or Scissors (s): ").lower()
 
+        # Check if the entry is valid or not
         if player not in choices:
             print("Invalid choice. Please enter 'r', 'p', or 's'.")
             continue
 
+        # Computer to randomnly select their choice
         computer_choice = random.choice(list(choices))
 
         print(
             f"You chose {choices[player]}, computer chose {choices[computer_choice]}.")
 
+        # Call to function to find winner after selections are made
         result = get_winner(player, computer_choice)
         print(result)
 
@@ -39,3 +46,58 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+"""
+
+
+Add Mosh's code:
+import random
+
+ROCK = 'r'
+SCISSORS = 's'
+PAPER = 'p'
+emojis = { ROCK: '🪨', SCISSORS: '✂️', PAPER: '📃' }
+choices = tuple(emojis.keys())
+
+def get_user_choice():
+  while True:
+    user_choice = input('Rock, paper, or scissors? (r/p/s): ').lower()
+    if user_choice in choices:
+      return user_choice      
+    else:
+      print('Invalid choice!')
+      
+def display_choices(user_choice, computer_choice):
+  print(f'You chose {emojis[user_choice]}')
+  print(f'Computer chose {emojis[computer_choice]}')
+
+
+def determine_winner(user_choice, computer_choice):
+  if user_choice == computer_choice:
+    print('Tie!')
+  elif (
+    (user_choice == ROCK and computer_choice == SCISSORS) or 
+    (user_choice == SCISSORS and computer_choice == PAPER) or 
+    (user_choice == PAPER and computer_choice == ROCK)):
+    print('You win')
+  else:
+    print('You lose')  
+
+def play_game():
+  while True:
+    user_choice = get_user_choice()
+
+    computer_choice = random.choice(choices)
+
+    display_choices(user_choice, computer_choice)
+
+    determine_winner(user_choice, computer_choice)
+
+    should_continue = input('Continue? (y/n): ').lower()
+    if should_continue == 'n':
+      break
+
+play_game()
+
+"""
