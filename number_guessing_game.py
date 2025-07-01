ed5be91400742f934f2943169f3293be577906a8
Praneet Kala <pj.kala333@gmail.com>
2025-07-01 13:19:21 +0200

    Number guessing game - My final code and Mosh's code

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,7 +2,6 @@ import random
 
 random_numb = random.randint(1, 100)  # Generate a random number
 while True:  # Loop until correct number found
-    # Ask for number
     guess_numb = int(input("Guess the number between 1 and 100: "))
     if guess_numb > random_numb:  # Check if higher
         print("Too high!")  # Message sayng number to high
@@ -11,3 +10,24 @@ while True:  # Loop until correct number found
     else:
         print("Congratulations! You guess the number")  # Correct number
         break
+
+"""
+It's a good idead to add a try catch to ensure users don't use ltters
+
+Here is Mosh's code:
+
+while True:
+    try:
+        guess = int(input('Guess the number between 1 and 100:'))
+    
+        if guess < number_to_guess: 
+            print('Too low!')
+        elif guess > number_to_guess:
+            print('Too high!')
+        else:
+            print('Congratulations! You guessed the number.')
+            break
+    except ValueError:
+        print(Please enter a valid number')
+
+"""
