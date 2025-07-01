22fb3463cfe92cceb52d6cff95e4aed2aa7e7740
Praneet Kala <pj.kala333@gmail.com>
2025-07-01 13:19:59 +0200

    Number guessing game - Final edits and working code

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -16,18 +16,22 @@ It's a good idead to add a try catch to ensure users don't use ltters
 
 Here is Mosh's code:
 
+import random 
+
+number_to_guess = random.randint(1, 100)
+
 while True:
-    try:
-        guess = int(input('Guess the number between 1 and 100:'))
-    
-        if guess < number_to_guess: 
-            print('Too low!')
-        elif guess > number_to_guess:
-            print('Too high!')
-        else:
-            print('Congratulations! You guessed the number.')
-            break
-    except ValueError:
-        print(Please enter a valid number')
+  try:
+    guess = int(input('Guess the number between 1 and 100: '))
+
+    if guess < number_to_guess:
+      print('Too low!')
+    elif guess > number_to_guess:
+      print('Too high!')
+    else:
+      print('Congratulations! You guessed the number.')
+      break
+  except ValueError:
+    print('Please enter a valid number')
 
 """
